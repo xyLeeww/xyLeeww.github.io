d1053b5d8cfa2db55be3af8103a6405619cba492
xyLeeww <xinyuleeww@gmail.com>
2026-07-07 13:49:19 +0800

    Add Katex support for markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { unified } from '@astrojs/markdown-remark';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://xyLeeww.github.io',
   output: 'static',
   server: { port: 3000 },
+  markdown: {
+    processor: unified({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+    }),
+  },
 });
